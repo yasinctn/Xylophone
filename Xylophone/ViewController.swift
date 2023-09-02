37003a1bd92113743e2a4b11459461d75272dafb
Yasin <yasincetin2014@gmail.com>
2023-09-02 14:49:48 +0300

    play sound feature completed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    
+    private var player = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func keyTapped(_ sender: UIButton) {
+        if let buttonName = sender.titleLabel?.text {
+            do{
+                try playAudio(name: buttonName)
+            } catch {
+                print("error")
+            }
+        }
+        
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.15) {
+            sender.alpha = 1.0
+        }
+    }
+    
+    
+    func playAudio(name:String) throws {
+        
+        if let url = Bundle.main.url(forResource: name, withExtension: "wav") {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.play()
+        }
+    }
 
 }
 
